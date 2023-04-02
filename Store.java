cf7e200e7a0d392df0b5a61a417d6ad45ad2fa02
braun43 <129580347+braun43@users.noreply.github.com>
2023-04-02 16:02:15 -0400

    Update Store.java

--- Store.java
+++ Store.java
@@ -35,38 +35,6 @@ public class Store {
         this.products = products;
     }
 
-    // the customer wil enter an input that we can convert to a boolean, stating that they want to buy a specific product
-    // this method can be used in conjunction with the shopping cart selection potentially
-    public void sellProduct(boolean buyProduct, Product product) {
-        if (buyProduct) {
-            for (int i = 0; i < products.size(); i++) {
-                if (products.get(i).equals(product)) {
-                    if (inStock(product)) {
-                        product.setQuantityForPurchase(product.getQuantityForPurchase() - 1);
-                        System.out.println("Thank you for your purchase!");
-                        // I think we want to put something here that adds the product to the user's purchases
-
-                    } else if (!inStock(product)) {
-                        System.out.println("This product(car model) is out of stock!");
-                    } else if (inStock(product) && product.getQuantityForPurchase() == 1) {
-                        product.setQuantityForPurchase(product.getQuantityForPurchase() - 1);
-                        products.remove(product);
-                        System.out.println("That was our last one!");
-                    }
-                }
-            }
-        } else {
-            System.out.println("Thank you for your interest in our product(car)!");
-        }
-        // I might need to add an exception where if the user tries to buy a product that doesn't exist in the store,
-        // an error message is thrown
-    }
-
-    // this method could be used to determine if a certain product in the store object is in/out of stock
-    public boolean inStock(Product product) {
-        return product.getQuantityForPurchase() > 0;
-    }
-
     // this method will calculate the sales made, so this method will only be called after product(s) have been sold
     public double calculateSales() {
         return 0;
